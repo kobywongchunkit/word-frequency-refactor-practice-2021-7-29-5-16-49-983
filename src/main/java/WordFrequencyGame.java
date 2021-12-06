397172bd6d9232ad2e97cd6bcc06d20ba59fbc06
kobywongchunkit <kobywongchunkit@gmail.com>
2021-12-06 16:43:22 +0800

    [WordFrequencyGame][Koby] refactor: Rename w1 to word1

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -36,7 +36,7 @@ public class WordFrequencyGame {
                 }
                 wordInfoList = list;
 
-                wordInfoList.sort((w1, w2) -> w2.getCount() - w1.getCount());
+                wordInfoList.sort((word1, w2) -> w2.getCount() - word1.getCount());
 
                 StringJoiner joiner = new StringJoiner("\n");
                 for (WordInfo w : wordInfoList) {
