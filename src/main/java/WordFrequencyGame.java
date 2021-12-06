64432b4c09044458948bf1e912e097e54db65bdf
kobywongchunkit <kobywongchunkit@gmail.com>
2021-12-06 17:08:12 +0800

    [WordFrequencyGame][Koby] refactor: remove deprecated method getListMap

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -44,23 +44,4 @@ public class WordFrequencyGame {
         return wordInfos;
     }
 
-
-    private Map<String, List<WordInfo>> getListMap(List<WordInfo> wordInfoList) {
-        Map<String, List<WordInfo>> map = new HashMap<>();
-        for (WordInfo wordInfo : wordInfoList) {
-//       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
-            if (!map.containsKey(wordInfo.getWord())) {
-                ArrayList arr = new ArrayList<>();
-                arr.add(wordInfo);
-                map.put(wordInfo.getWord(), arr);
-            } else {
-                map.get(wordInfo.getWord()).add(wordInfo);
-            }
-        }
-
-
-        return map;
-    }
-
-
 }
