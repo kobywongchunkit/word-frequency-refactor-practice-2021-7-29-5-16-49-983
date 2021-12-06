b71493510de0bce0329cfa4f95b30bb055155c74
kobywongchunkit <kobywongchunkit@gmail.com>
2021-12-06 17:06:50 +0800

    [WordFrequencyGame][Koby] refactor: rewrite the calculateWordFrequency method

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -1,8 +1,5 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.StringJoiner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class WordFrequencyGame {
 
@@ -36,26 +33,15 @@ public class WordFrequencyGame {
         }
     }
 
-    private List<WordInfo> calculateWordFrequency(String inputStr) {
-        //split the input string with 1 to n pieces of spaces
-        String[] words = inputStr.split(SPACE_PATTERN);
-
-        List<WordInfo> wordInfoList = new ArrayList<>();
-        for (String word : words) {
-            WordInfo wordInfo = new WordInfo(word, 1);
-            wordInfoList.add(wordInfo);
-        }
-
-        //get the map for the next step of sizing the same word
-        Map<String, List<WordInfo>> map = getListMap(wordInfoList);
-
-        List<WordInfo> list = new ArrayList<>();
-        for (Map.Entry<String, List<WordInfo>> entry : map.entrySet()) {
-            WordInfo wordInfo = new WordInfo(entry.getKey(), entry.getValue().size());
-            list.add(wordInfo);
-        }
-        wordInfoList = list;
-        return wordInfoList;
+    private List<WordInfo>calculateWordFrequency(String sentence){
+        List<String> words = Arrays.asList(sentence.split(SPACE_PATTERN));
+        List<String> distinctWords = words.stream().distinct().collect(Collectors.toList());
+        List<WordInfo> wordInfos = new ArrayList<>();
+        distinctWords.forEach( distinctWord -> {
+            int frequency = (int) words.stream().filter(word -> word.equals(distinctWord)).count();
+            wordInfos.add(new WordInfo(distinctWord, frequency)) ;
+        });
+        return wordInfos;
     }
 
 
